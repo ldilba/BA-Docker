ae13147c7ecb99e513bad3044e4979ce5c82efb0
Laurenz Dilba <laurenz.dilba@hotmail.de>
2022-11-18 10:16:46 +0100

    added core & sql build file

---
+++ setup.sql
@@ -0,0 +1,24 @@
+create database db_logs;
+
+use db_logs;
+
+create table logs
+(
+    id        int auto_increment,
+    level     varchar(255)  null,
+    message   varchar(2000) null,
+    timestamp timestamp     null,
+    uid       varchar(255)  null,
+    constraint logs_pk
+        primary key (id)
+);
+
+create table services
+(
+    id           int auto_increment,
+    service      varchar(255) null,
+    display_name varchar(255) null,
+    constraint services_pk
+        primary key (id)
+);
+
